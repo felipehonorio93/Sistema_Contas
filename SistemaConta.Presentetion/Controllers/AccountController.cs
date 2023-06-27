b33d180b23d8d9c4a63b59014ae469b24184834d
felipehonorio93 <felipehonoriomoraes01@gmail.com>
2023-06-27 16:48:37 -0300

    first commit

---
+++ SistemaConta.Presentetion/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaConta.Presentetion.Models;
+
+namespace SistemaConta.Presentetion.Controllers
+{
+    public class AccountController : Controller
+    {
+        //GET: Account/Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+        //POST: Account/Login
+        [HttpPost]
+        public IActionResult Login(AccountLoginViewModel model)
+        {
+            return View();
+
+        }
+
+        //GET: Account/Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        //POST: Account/Register
+        [HttpPost]
+        public IActionResult Register(AccountRegisterViewModel model)
+        {
+            return View();
+        }
+
+        //GET: Account/PasswordRecover
+        public IActionResult PasswordRecover()
+        {
+            return View();
+        }
+
+        //POST: Account/PasswordRecover
+        [HttpPost]
+        public IActionResult PasswordRecover (AccountPasswordRecoverViewModel model)
+        {
+            return View();
+        }
+    }
+}
